bfaf8090fa5a0ec9a95140bd0f9989b502e0676c
Fernanda Barros <barroscnanda@gmail.com>
2021-07-30 19:26:31 -0300

    clean pselastic_secure_pos code

--- perfsonar-scripts/pselastic_secure_pos.sh
+++ perfsonar-scripts/pselastic_secure_pos.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
 # Configure index state management (ISM) policy for pscheduler indices
-echo "[Configure policy]"
-echo ${ADMIN_PASS}
-echo "[Trying to start elastic]"
-echo "[Status 1]"
-systemctl status elasticsearch
-echo "[Start]"
-systemctl start elasticsearch
-echo "[Sleeping]"
-sleep 100
-echo "[Status 2]"
-systemctl status elasticsearch
-echo "[Trying to create policy]"
-# Create policy
+echo "[Create policy]"
+# Create index policy
 curl -v -k -u admin:${ADMIN_PASS} -H 'Content-Type: application/json' -X PUT "https://localhost:9200/_opendistro/_ism/policies/policy_1" -d "@/usr/lib/perfsonar/archive/pselastic_setup/conf.d/ilm/install/pscheduler_default_policy.json"
 echo "[Applying policy]"
 # Apply policy to index
