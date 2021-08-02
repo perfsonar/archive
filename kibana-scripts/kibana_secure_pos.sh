68e51839f4f48ba908c3fcee2ff28f86a326568c
Luan Rios <etnioscs2@gmail.com>
2021-08-02 14:44:21 -0300

    Correcting default index pattern's properties

--- kibana-scripts/kibana_secure_pos.sh
+++ kibana-scripts/kibana_secure_pos.sh
@@ -4,6 +4,6 @@
 echo "[Add default index-pattern]"
 ADMIN_PASS=$(grep "admin " /etc/perfsonar/elastic/auth_setup.out | head -n 1 | sed 's/^admin //')
 KIBANA_VERSION=$(/usr/share/kibana/bin/kibana --config /etc/kibana/kibana.yml --allow-root --version)
-curl -v -4 -X POST -u admin:${ADMIN_PASS} http://localhost:5601/api/saved_objects/index-pattern -H "kbn-version: ${KIBANA_VERSION}" -H "kbn-xsrf: true" -H "content-type: application/json; charset=utf-8" -d '{"attributes":{"title":"pscheduler-*","timeFieldName":"start_time","fields":"[]"}}'
+curl -v -4 -X POST -u admin:${ADMIN_PASS} http://localhost:5601/api/saved_objects/index-pattern -H "kbn-version: ${KIBANA_VERSION}" -H "kbn-xsrf: true" -H "content-type: application/json; charset=utf-8" -d '{"attributes":{"title":"pscheduler*","timeFieldName":"pscheduler.start_time","fields":"[]"}}'
 echo "[DONE]"
 echo ""
