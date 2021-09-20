4bcadf995778d618fda7e06d0c7e05f54e06a58e
Daniel Neto <daniel.jsilvan@gmail.com>
2021-09-20 16:16:07 -0300

    Adding pscheduler index to ISM default policy.

--- perfsonar-scripts/pselastic_secure_pos.sh
+++ perfsonar-scripts/pselastic_secure_pos.sh
@@ -14,9 +14,9 @@ fi
 # Configure index state management (ISM) policy for pscheduler indices
 echo "[Create policy]"
 # Create index policy
-curl -v -k -u admin:${ADMIN_PASS} -H 'Content-Type: application/json' -X PUT "https://localhost:9200/_opendistro/_ism/policies/policy_1" -d "@/usr/lib/perfsonar/archive/pselastic_setup/conf.d/ilm/install/pscheduler_default_policy.json"
+curl -v -k -u admin:${ADMIN_PASS} -H 'Content-Type: application/json' -X PUT "https://localhost:9200/_opendistro/_ism/policies/pscheduler_default_policy" -d "@/usr/lib/perfsonar/archive/pselastic_setup/conf.d/ilm/install/pscheduler_default_policy.json"
 echo "[Applying policy]"
 # Apply policy to index
-curl -v -k -u admin:${ADMIN_PASS} -H 'Content-Type: application/json' -X POST "https://localhost:9200/_opendistro/_ism/add/pscheduler*" -d '{ "policy_id": "policy_1" }'
+curl -v -k -u admin:${ADMIN_PASS} -H 'Content-Type: application/json' -X POST "https://localhost:9200/_opendistro/_ism/add/pscheduler*" -d '{ "policy_id": "pscheduler_default_policy" }'
 echo "[DONE]"
 echo ""
