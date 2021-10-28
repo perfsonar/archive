0bbb46873a098929675475016024f8fbadfaa5a7
Daniel Neto <daniel.jsilvan@gmail.com>
2021-10-28 15:44:29 -0300

    Improving logging

--- perfsonar-scripts/pselastic_secure_pos.sh
+++ perfsonar-scripts/pselastic_secure_pos.sh
@@ -11,6 +11,7 @@ elif [ -z "$ADMIN_PASS" ]; then
     exit 1
 fi
 
+echo "Waiting elasticsearch API to start..."
 api_status=$(curl -s -o /dev/null -w "%{http_code}" -u admin:${ADMIN_PASS} -k https://localhost:9200/_cluster/health)
 i=0
 while [[ $api_status -ne 200 ]]
@@ -19,17 +20,20 @@ do
     ((i++))
     # Wait a maximum of 100 seconds for the API to start
     if [[ $i -eq 100 ]]; then
+        echo "[ERROR] API start timeout"
         exit 1
     fi
     api_status=$(curl -s -o /dev/null -w "%{http_code}" -u admin:${ADMIN_PASS} -k https://localhost:9200/_cluster/health)
 done
 
+echo "API started!"
+
 # Configure index state management (ISM) policy for pscheduler indices
 echo "[Create policy]"
 # Create index policy
 curl -k -u admin:${ADMIN_PASS} -H 'Content-Type: application/json' -X PUT "https://localhost:9200/_opendistro/_ism/policies/pscheduler_default_policy" -d "@/usr/lib/perfsonar/archive/pselastic_setup/conf.d/ilm/install/pscheduler_default_policy.json" 2>/dev/null
-echo "[Applying policy]"
+echo -e "\n[Applying policy]"
 # Apply policy to index
 curl -k -u admin:${ADMIN_PASS} -H 'Content-Type: application/json' -X POST "https://localhost:9200/_opendistro/_ism/add/pscheduler*" -d '{ "policy_id": "pscheduler_default_policy" }' 2>/dev/null
-echo "[DONE]"
+echo -e "\n[DONE]"
 echo ""
