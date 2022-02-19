a53a305731bf81eab8a77990ec24c7f9269b9648
Daniel Neto <daniel.jsilvan@gmail.com>
2022-02-18 23:36:05 -0300

    Log message updated

--- dashboards-scripts/dashboards_secure_pre.sh
+++ dashboards-scripts/dashboards_secure_pre.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configure opensearch dashboards to use new kibanaserver password
-echo "[Configure kibana]"
+echo "[Configure Opensearch-Dashboards]"
 DASHBOARDS_PASS=$(grep "kibanaserver " /etc/perfsonar/elastic/auth_setup.out | head -n 1 | sed 's/^kibanaserver //')
 sed -i "s/opensearch.password: \"kibanaserver\"/opensearch.password: \"${DASHBOARDS_PASS}\"/g" /etc/opensearch-dashboards/opensearch_dashboards.yml
 echo "[DONE]"
