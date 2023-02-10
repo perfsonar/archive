eb7447611be1583468f81cc3a572fd5948fb3096
Daniel Neto <daniel.jsilvan@gmail.com>
2023-02-10 17:05:20 -0300

    Updating dashboards configuration scripts

--- perfsonar-dashboards/perfsonar-dashboards/dashboards-scripts/dashboards_secure_pre.sh
+++ perfsonar-dashboards/perfsonar-dashboards/dashboards-scripts/dashboards_secure_pre.sh
@@ -3,8 +3,8 @@
 # Configure opensearch dashboards to use new kibanaserver password
 echo "[Configure Opensearch-Dashboards]"
 OPENSEARCHDASH_CONFIG=/etc/opensearch-dashboards/opensearch_dashboards.yml
-DASHBOARDS_PASS=$(grep "kibanaserver " /etc/perfsonar/opensearch/auth_setup.out | head -n 1 | sed 's/^kibanaserver //')
-sed -i "s/opensearch.password: kibanaserver/opensearch.password: ${DASHBOARDS_PASS}/g" $OPENSEARCHDASH_CONFIG
+DASHBOARDS_PASS=$(grep "kibanaserver " /etc/perfsonar/opensearch/auth_setup.out | head -n 1 | awk '{print $2}')
+sed -i "s/^opensearch.password: kibanaserver/opensearch.password: ${DASHBOARDS_PASS}/g" $OPENSEARCHDASH_CONFIG
 # Clear and then set reverse proxy settings
 sed -i '/^server.basePath:.*/d' $OPENSEARCHDASH_CONFIG
 sed -i '/^server.host:.*/d' $OPENSEARCHDASH_CONFIG
