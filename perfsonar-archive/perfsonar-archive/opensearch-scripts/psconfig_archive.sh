f1b1e20ec8537c5156cfb67d7cd559a356907ac2
Andy Lake <andy@es.net>
2023-04-05 09:47:56 -0400

    Updating script that generates archive definitions to be easier to use in different scenarios

--- perfsonar-archive/perfsonar-archive/opensearch-scripts/psconfig_archive.sh
+++ perfsonar-archive/perfsonar-archive/opensearch-scripts/psconfig_archive.sh
@@ -4,22 +4,42 @@
 # Helper script to output a psConfig archive definition. 
 #
 # Params:
-#   hostname: A vaid hostname of the archive server. Defaults to psconfig variable {% scheduled_by_address %}
+#   -n hostname: A vaid hostname of the archive server. Defaults to psconfig variable {% scheduled_by_address %}
+#   -a authtype: Authorization. Valid values are 'basic' and 'none'. Default is 'basic'.
 ####
 
-ARCHIVE_HOST=${1:-'{% scheduled_by_address %}'}
-AUTH_HEADER=`cat /etc/perfsonar/logstash/proxy_auth.json`
+DEFAULT_ARCHIVE_HOST='{% scheduled_by_address %}'
+AUTH_TYPE='basic'
+while getopts n:a: flag
+do
+    case "${flag}" in
+        n) ARCHIVE_HOST=${OPTARG};;
+        a) AUTH_TYPE=${OPTARG};;
+    esac
+done
+
+#determin auth strategy
+AUTH_HEADER=""
+if [ "$AUTH_TYPE" == "basic" ]; then
+    AUTH_HEADER=`cat /etc/perfsonar/logstash/proxy_auth.json`
+    AUTH_HEADER=`printf ",\n            $AUTH_HEADER"`
+fi
+
+
+#check if this is a remote MA
+if [ -n "$ARCHIVE_HOST" ]; then
+
 cat << EOF
 {
     "archiver": "http",
     "data": {
-        "schema": 2,
+        "schema": 3,
         "_url": "https://${ARCHIVE_HOST}/logstash",
+        "verify-ssl": false,
         "op": "put",
         "_headers": {
             "x-ps-observer": "{% scheduled_by_address %}",
-            "content-type": "application/json", 
-            ${AUTH_HEADER}
+            "content-type": "application/json"${AUTH_HEADER}
         },
         "_meta": {
             "esmond_url": "https://${ARCHIVE_HOST}/esmond/perfsonar/archive/"
@@ -28,3 +48,21 @@ cat << EOF
 }
 EOF
 
+else
+
+cat << EOF
+{
+    "archiver": "http",
+    "data": {
+        "schema": 2,
+        "_url": "https://${DEFAULT_ARCHIVE_HOST}/logstash",
+        "op": "put",
+        "_headers": {
+            "x-ps-observer": "{% scheduled_by_address %}",
+            "content-type": "application/json"${AUTH_HEADER}
+        }
+    }
+}
+EOF
+
+fi
