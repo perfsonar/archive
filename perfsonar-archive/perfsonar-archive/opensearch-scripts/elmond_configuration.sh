028f3126a220d2411df53c715885b86ae6e90563
Mark Feit <mfeit@internet2.edu>
2025-03-12 11:37:15 +0000

    Revert out Rezi's commits

--- perfsonar-archive/perfsonar-archive/opensearch-scripts/elmond_configuration.sh
+++ perfsonar-archive/perfsonar-archive/opensearch-scripts/elmond_configuration.sh
@@ -1,18 +1,18 @@
-OPENSEARCH_PASSWORD_FILE=/etc/perfsonar/opensearch/auth_setup.out
+ELASTIC_PASSWORD_FILE=/etc/perfsonar/opensearch/auth_setup.out
 ELMOND_CONFIG_FILE=/etc/perfsonar/elmond/elmond.json
 TEMP_DIR=/tmp
 
-READER_PW=$(grep pscheduler_reader ${OPENSEARCH_PASSWORD_FILE} | awk '{print $2}' | head -1)
-jq --arg opensearchURL  "https://pscheduler_reader:${READER_PW}@localhost:9200" '.OPENSEARCH_HOSTS[0] = $opensearchURL' ${ELMOND_CONFIG_FILE} > ${TEMP_DIR}/elmond_config_temp.json
+READER_PW=$(grep pscheduler_reader ${ELASTIC_PASSWORD_FILE} | awk '{print $2}' | head -1)
+jq --arg elasticURL  "https://pscheduler_reader:${READER_PW}@localhost:9200" '.ELASTIC_HOSTS[0] = $elasticURL' ${ELMOND_CONFIG_FILE} > ${TEMP_DIR}/elmond_config_temp.json
 
 mv -f ${TEMP_DIR}/elmond_config_temp.json ${ELMOND_CONFIG_FILE}
 
 jq --arg rootCert   '/etc/opensearch/root-ca.pem' \
    '.use_ssl = true | .ca_certs = $rootCert' \
-   <<< {} > ${TEMP_DIR}/opensearch_params_temp.json
+   <<< {} > ${TEMP_DIR}/elastic_params_temp.json
 
-jq '.OPENSEARCH_PARAMS = $params' --argjson params "$(<${TEMP_DIR}/opensearch_params_temp.json)" ${ELMOND_CONFIG_FILE} > ${TEMP_DIR}/elmond_config_temp.json
+jq '.ELASTIC_PARAMS = $params' --argjson params "$(<${TEMP_DIR}/elastic_params_temp.json)" ${ELMOND_CONFIG_FILE} > ${TEMP_DIR}/elmond_config_temp.json
 
 mv -f ${TEMP_DIR}/elmond_config_temp.json ${ELMOND_CONFIG_FILE}
 
-rm -f ${TEMP_DIR}/opensearch_params_temp.json
+rm -f ${TEMP_DIR}/elastic_params_temp.json
